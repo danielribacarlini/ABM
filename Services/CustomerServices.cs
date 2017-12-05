9c96c231a88af73a003abe1091722860dd5189e4
WIN10NET-19\Usuario <danielribacarlini@gmail.com>
2017-12-05 11:46:16 -0300

    first Comit

---
+++ Services/CustomerServices.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess1;
+using Services.Model;
+
+namespace Services
+{
+    public class CustomerServices
+    {
+        private Repository<Customer> _customerRepository;
+
+        public CustomerServices()
+        {
+            _customerRepository = new Repository<Customer>();
+        }
+
+        public List<Customer> GetAll()
+        {
+            
+                var customers = _customerRepository.Set().Select(c=> new Customer
+                {
+                    CustomerID = c.CustomerID,
+                    CompanyName = c.CompanyName,
+                    ContactName = c.ContactName,
+                    City = c.City
+                }).ToList();
+                return customers;
+            
+        }
+
+        public Customer GetOne(string id)
+        {
+          
+                var customers = _customerRepository.Set().FirstOrDefault(c => c.CustomerID == id);
+                var customer = new Customer
+                {
+                    CustomerID = customers.CustomerID,
+                    CompanyName = customers.CompanyName,
+                    ContactName = customers.CompanyName,
+                    City = customers.City
+                };
+
+                return customer;
+                        
+        }
+
+        public void Update(Customer customerM)
+        {
+            
+                var customers = _customerRepository.Set().FirstOrDefault(c => c.CustomerID == customerM.CustomerID);
+
+                customers.CustomerID = customerM.CustomerID;
+                customers.CompanyName = customerM.CompanyName;
+                customers.ContactName = customerM.ContactName;
+                customers.City = customerM.City;
+
+                _customerRepository.SaveChanges();
+        }
+
+        
+
+        public bool Delete(string id)
+        {
+            
+                var customer = _customerRepository.Set().FirstOrDefault(c => c.CustomerID == id);
+
+                if (customer == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (context.Orders.Any(p => p.CustomerID == id))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        context.Customers.Remove(customer);
+                        context.SaveChanges();
+                        return true;
+                    }
+                }
+            
+        }
+
+        public void Save(Customer newCustomer)
+        {
+            using(var context = new Context())
+            {
+                var customer = new Customers
+                {
+                    CustomerID = newCustomer.CustomerID,
+                    CompanyName = newCustomer.CompanyName,
+                    ContactName = newCustomer.ContactName,
+                    ContactTitle = "",
+                    Address = "",
+                    City = newCustomer.City,
+                    Region = "",
+                    PostalCode = "",
+                    Country = "",
+                    Phone = "",
+                    Fax = ""
+
+                };
+
+                try
+                {
+                    context.Customers.Add(customer);
+                    context.SaveChanges();
+                  
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+        }
+    }
+}
---
+++ UI.Consola/Program.cs
@@ -0,0 +1,161 @@
+﻿using Services;
+using Services.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var op = "";
+            do
+            {
+                Console.WriteLine("Menu\n=====================================================================");
+                Console.WriteLine("L - Listar \nM - Modificar \nD - Borrar \nC - Nuevo \nS - Salir");
+
+                var band = true;
+
+                do
+                {
+                    op = Console.ReadLine();
+                    if (op != "M" && op != "D" && op != "C" && op != "S" && op != "L")
+                    {
+                        Console.WriteLine("ingrese valor correcto");
+                        band = true;
+                    }
+                    else
+                    {
+                        band = false;
+                    }
+                } while (band);
+
+                switch (op)
+                {
+                    case "M":
+                        Edit();
+                        break;
+                    case "D":
+                        Delete();
+                        break;
+                    case "C":
+                        Create();
+                        break;
+                    case "L":
+                        Listar();
+                        break;
+                    default:
+                        break;
+                }
+            } while (op != "S");
+        }
+
+        public static string ReadId()
+        {
+            Console.WriteLine("Ingrese Id Categoria");
+            var id = "";
+            var band = true;
+            do
+            {
+                id = Console.ReadLine();
+                /*
+                if (id < 0 )
+                {
+                    Console.WriteLine("Ingrese Valor Correcto");
+                }
+                else
+                {
+                    band = false;
+                }
+                */
+            } while (!band);
+            return id;
+        }
+
+        public static void Edit()
+        {
+            
+            var customerServices = new CustomerServices();
+                var id = ReadId();
+
+                var band1 = true;
+                var customer = customerServices.GetOne(ReadId());
+                do
+                {
+                    
+                    if (customer != null)
+                    {
+                        
+                        band1 = false;
+                        Console.WriteLine("Ingrese nuevo Numbre compania");
+                        customer.CompanyName = Console.ReadLine();
+                        Console.WriteLine("inrgese nombre del contacto");
+                        customer.ContactName = Console.ReadLine();
+                        Console.WriteLine("Ingrese nombre ciudad");
+                        customer.City = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("No existe cliente");
+                        id = ReadId();
+                    }
+
+                } while (band1);
+            customerServices.Update(customer);
+        }
+
+        public static void Delete()
+        {
+            var customerServices = new CustomerServices();
+            var id = ReadId();
+            if (customerServices.Delete(id))
+            {
+                Console.WriteLine("Cliente Borrado correctamente");
+            }
+            else
+            {
+                Console.WriteLine("No se pudo borrar el cliente");
+            }
+                           
+        }
+
+        public static void Listar()
+        {
+            var customerServices = new CustomerServices();
+            var customers = customerServices.GetAll();
+
+            foreach (var item in customers)
+            {
+                Console.WriteLine($"{item.CustomerID} {item.CompanyName} {item.ContactName} {item.City}");
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        public static void Create()
+        {
+                var customerServices = new CustomerServices();
+                var newCustomer = new Customer();
+
+                Console.WriteLine("ingrese ID");
+                newCustomer.CustomerID = Console.ReadLine();
+                Console.WriteLine("ingrese nombre de la nombre de la compania");
+                newCustomer.CompanyName = Console.ReadLine();
+                Console.WriteLine("ingrese nombre de contacto");
+                newCustomer.ContactName = Console.ReadLine();
+                Console.WriteLine("ingrese ciudad:");
+                newCustomer.City = Console.ReadLine();
+
+                customerServices.Save(newCustomer);
+
+                Console.WriteLine("Categoria guardada correctamente \n\n");
+        }
+
+
+
+    }
+}
+
